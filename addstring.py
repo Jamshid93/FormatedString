0f441c65b1134716c785a74bea2e62cb89080642
Jamshid93 <hamzayevjf@gmail.com>
2019-07-14 23:53:31 +0500

    This example formatted string 2

---
+++ addstring.py
@@ -0,0 +1,6 @@
+good=" love "
+bad= " lie "
+clever_idea= good + " is good way it with will be nice but with " + bad + " will not be ."
+#print(clever_idea)
+clv_id=f'{good}  is good way it with will be nice but with  {bad}  will not be'
+print(clv_id)
