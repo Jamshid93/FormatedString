8caecf719cf4a1b4de9abe5069b5e25e07119fc6
Jamshid93 <hamzayevjf@gmail.com>
2019-07-14 23:43:40 +0500

    Formated String

---
+++ Forstring.py
@@ -0,0 +1,6 @@
+last="Jhon"
+first="Smith"
+message=first + '[ ' + last + ']  is coder  ' # this  code  concenating
+#print(message)
+msg= f'{last} [{first}] is a coder' #this code formated 
+print(msg)
